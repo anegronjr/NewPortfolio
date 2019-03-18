a71a3587bcd7b7e886752e9c5f1d0bec9c3b45d6
Artie Negron <anegroncsu@gmail.com>
2019-03-17 23:07:44 -0400

    Finished rough Porjects Section.

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ heroName.style.opacity = "1";
 heroName.style.transform = "translateX(0px)";
 heroBlurb.style.opacity = "1";
 heroBlurb.style.transform = "translateY(0px)";
+
+const projContainers = document.querySelectorAll(".project-container");
+const projTracks = document.querySelectorAll(".project-track");
+
+for (let i = 0; i < projContainers.length; i++){
+    projContainers[i].addEventListener("click", function(){
+        if (projTracks[i].style.transform == "translateX(-100%)"){
+        projTracks[i].style.transform = "translateX(0)";
+        } else {
+            projTracks[i].style.transform = "translateX(-100%)";
+        }
+    });
+}
